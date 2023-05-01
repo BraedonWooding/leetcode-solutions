90542f54be270854497f4fb9decef1fe201db718
Braedon <Braedonww@gmail.com>
2023-05-01 14:12:36 +1000

    Time: 7 ms (41.04%) | Memory: 2.3 MB (12.93%) - LeetSync

--- roman-to-integer/roman-to-integer.rs
+++ roman-to-integer/roman-to-integer.rs
@@ -4,42 +4,56 @@ impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
         let mut result: i32 = 0;
 
-        let mut valueMap = HashMap::<char, i32>::new();
-        valueMap.insert('I', 1);
-        valueMap.insert('V', 5);
-        valueMap.insert('L', 50);
-        valueMap.insert('X', 10);
-        valueMap.insert('C', 100);
-        valueMap.insert('D', 500);
-        valueMap.insert('M', 1000);
-
         // we can iterate 2 characters at a time to handle the IV/IX/... case
         let mut iter = s.chars().peekable();
 
         while let Some(firstChar) = iter.next() {
-            let firstValue = valueMap[&firstChar];
-            if let Some(secondChar) = iter.peek() {
-                let secondValue = valueMap[&secondChar];
-                if firstValue == secondValue {
-                    // since it's ordered in descending order
-                    // we can never have a value larger than firstValue
-                    // since we already have seen it twice
-                    result += firstValue + secondValue;
-                    iter.next();
-                } else if firstValue < secondValue {
-                    // IV case
-                    result += secondValue - firstValue;
-                    iter.next();
-                } else {
-                    // VI case, we can't just add firstValue & secondValue
-                    // since it could be something like XIV
-                    // we don't do iter.next() since we want to keep the next value in the iterator
-                    // so we'll process it next time we can just fallback to if it's a single value
-                    result += firstValue;
-                }
-            } else {
-                // just a single value
-                result += firstValue;
+            result += match firstChar {
+                // before V & X
+                'I' => match iter.peek() {
+                    Some('V') => {
+                        iter.next();
+                        5 - 1
+                    },
+                    Some('X') => {
+                        iter.next();
+                        10 - 1
+                    },
+                    _ => 1,
+                },
+                // 5/50/... can't be before other characters
+                'V' => 5,
+                // before L & C
+                'X' => match iter.peek() {
+                    Some('L') => {
+                        iter.next();
+                        50 - 10
+                    },
+                    Some('C') => {
+                        iter.next();
+                        100 - 10
+                    },
+                    _ => 10,
+                },
+                // 5/50/... can't be before other characters
+                'L' => 50,
+                // can be before D or M
+                'C' => match iter.peek() {
+                    Some('D') => {
+                        iter.next();
+                        500 - 100
+                    },
+                    Some('M') => {
+                        iter.next();
+                        1000 - 100
+                    },
+                    _ => 100,
+                },
+                // 5/50/... can't be before other characters
+                'D' => 500,
+                // no larger value so we can just always output this
+                'M' => 1000,
+                _ => 0,
             }
         }
 
