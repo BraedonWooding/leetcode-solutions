6b208238b6c91567aead9327e1e37fc1afeebfbf
Braedon <Braedonww@gmail.com>
2023-05-01 14:55:19 +1000

    Time: 0 ms (100.00%) | Memory: 2.3 MB (16.67%) - LeetSync

--- average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.rs
+++ average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.rs
@@ -2,6 +2,7 @@ impl Solution {
     pub fn average(mut salary: Vec<i32>) -> f64 {
         // use the fact that a sorted list of integers will have max/min at extremes
         salary.sort();
-        return (salary.iter().skip(1).take(salary.len() - 2).sum::<i32>() as f64) / (salary.len() as f64 - 2.0);
+        let len = salary.len() - 2;
+        return (salary[1..=len].iter().sum::<i32>() as f64) / (len as f64);
     }
 }
