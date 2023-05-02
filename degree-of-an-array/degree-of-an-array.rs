96c1a7d6307ed445807f130b410bfaae4d441590
Braedon <Braedonww@gmail.com>
2023-05-02 17:37:39 +1000

    Time: 7 ms (25.00%) | Memory: 2.7 MB (25.00%) - LeetSync

--- degree-of-an-array/degree-of-an-array.rs
+++ degree-of-an-array/degree-of-an-array.rs
@@ -11,17 +11,23 @@ impl Solution {
     pub fn find_shortest_sub_array(nums: Vec<i32>) -> i32 {
         if (nums.len() == 0) { return 0; }
 
+        let mut max = 1;
+
         let degrees = nums
             .iter()
             .enumerate()
             .fold(HashMap::<i32, Num>::new(), |mut map, (i, val)|{
                 map.entry(*val)
-                    .and_modify(|f| { f.count += 1; f.last_index = i })
+                    .and_modify(|f| {
+                        f.count += 1;
+                        f.last_index = i;
+                        if f.count > max { max = f.count; }
+                    })
                     .or_insert(Num{ count: 1, first_index: i, last_index: i });
                 map
             });
 
-        let max = degrees.values().max_by_key(|num| num.count).unwrap().count;
+        // let max = degrees.values().max_by_key(|num| num.count).unwrap().count;
         let minSubArray = degrees.values()
             .filter(|num| num.count == max)
             .min_by_key(|num| num.last_index - num.first_index)
