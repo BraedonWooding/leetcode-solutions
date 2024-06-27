d3b260475d01018692d592e58a13ab0468c1ebed
Braedon <Braedonww@gmail.com>
2024-06-27 21:13:39 +1000

    Time: 5 ms (28.14%) | Memory: 11 MB (71.93%) - LeetSync

---
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // we can loop backwards through the arrays using the fact nums1
+        // has enough space to store m + n
+        int i = m - 1, j = n - 1;
+        int endBuf = m + n - 1; 
+        while (i >= 0) {
+            if (j >= 0 && nums2[j] > nums1[i]) {
+                nums1[endBuf--] = nums2[j--];
+            } else {
+                nums1[endBuf--] = nums1[i--];
+            }
+        }
+        // handle the last of j
+        while (j >= 0) nums1[endBuf--] = nums2[j--];
+    }
+};
