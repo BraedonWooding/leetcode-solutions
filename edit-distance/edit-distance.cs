341c24a52373139074e25bf9ff544a5467ccc630
Braedon <Braedonww@gmail.com>
2024-06-28 21:25:11 +1000

    Time: 142 ms (5.07%) | Memory: 45.9 MB (21.89%) - LeetSync

--- edit-distance/edit-distance.cs
+++ edit-distance/edit-distance.cs
@@ -3,11 +3,12 @@ public class Solution {
         // create a 2d array that stores the cost of operations to get to a point
         // we can then use DP to calculate the cost, with the result being the bottom right corner (max row/col)
         var dp = new int[word1.Length + 1, word2.Length + 1];
-        for (int i = 0; i <= word1.Length; i++) {
-            for (int j = 0; j <= word2.Length; j++) {
-                if (j == 0) dp[i, j] = i;
-                else if (i == 0) dp[i, j] = j;
-                else dp[i, j] = Math.Min(
+        for (int i = 0; i <= word1.Length; i++) dp[i, 0] = i;
+        for (int i = 0; i <= word2.Length; i++) dp[0, i] = i;
+
+        for (int i = 1; i <= word1.Length; i++) {
+            for (int j = 1; j <= word2.Length; j++) {
+                dp[i, j] = Math.Min(
                     Math.Min(
                         dp[i - 1, j] + 1, // Deletion
                         dp[i, j - 1] + 1  // Insertion
