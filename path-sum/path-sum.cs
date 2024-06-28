977a02bb8bf5ac210e760dc4bd61e5cd3130b527
Braedon <Braedonww@gmail.com>
2024-06-28 18:41:22 +1000

    Time: 86 ms (30.68%) | Memory: 45.5 MB (14.97%) - LeetSync

---
+++ path-sum/path-sum.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        // base case: empty tree has path sum if = 0
+        if (root == null) return false;
+        // leaf node
+        if (root.left == null && root.right == null) return targetSum == root.val;
+        
+        // target sum can be negative so we shouldn't do a sort of "optimization" to skip iteration if < 0
+
+        // else we subtract our current node from the target sum then check either paths
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+}
