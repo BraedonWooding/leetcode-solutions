d367ea130c50fa8143737626648834e7e25b4a2b
Braedon <Braedonww@gmail.com>
2024-06-30 02:02:47 +1000

    Time: 322 ms (11.90%) | Memory: 69.8 MB (10.20%) - LeetSync

---
+++ maximal-square/maximal-square.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int MaximalSquare(char[][] matrix) {
+        // re-use the matrix for this for a cheeky O(1)
+        int m = matrix[0].Length;
+        int max = 0;
+        for (int i = matrix.Length - 1; i >= 0; i--) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    // no point updating cells that aren't 1
+                    int val = Math.Min(
+                        j > 0 ? (matrix[i][j - 1] - '0') : 0,
+                        Math.Min(
+                            i < matrix.Length - 1 ? (matrix[i + 1][j] - '0') : 0,
+                            i < matrix.Length - 1 && j > 0 ? (matrix[i + 1][j - 1] - '0') : 0
+                        )
+                    ) + 1;
+                    matrix[i][j] = (char)('0' + val);
+                    if (val > max) max = val;
+                }
+                Console.Write(matrix[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        return max * max;
+    }
+}
