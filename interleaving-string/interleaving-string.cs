7f4678ae04aa6241bf6e8a275f9de00e81076eb9
Braedon <Braedonww@gmail.com>
2024-06-29 17:35:44 +1000

    Time: 48 ms (91.23%) | Memory: 40.1 MB (71.05%) - LeetSync

--- interleaving-string/interleaving-string.cs
+++ interleaving-string/interleaving-string.cs
@@ -5,17 +5,9 @@ public class Solution {
         var dp = new bool[s1.Length + 1, s2.Length + 1];
         // base case empty strings work
         dp[0, 0] = true;
-        // for (int i = 1; i <= s1.Length && s3[i - 1] == s1[i - 1]; i++) {
-        //     dp[i, 0] = true;
-        // }
-        // for (int i = 1; i <= s2.Length && s3[i - 1] == s2[i - 1]; i++) {
-        //     dp[0, i] = true;
-        // }
 
         for (int i = 0; i <= s1.Length; i++) {
             for (int j = 0; j <= s2.Length; j++) {
-                if (i == 0 && j == 0) continue;
-
                 if (i > 0 && s3[i + j - 1] == s1[i - 1]) {
                     dp[i, j] = dp[i - 1, j];
                 }
