a6f043e867f73cd5dadd901d19463db819bbf651
Braedon <Braedonww@gmail.com>
2024-06-29 16:38:15 +1000

    Time: 19 ms (79.62%) | Memory: 26.9 MB (38.47%) - LeetSync

--- number-of-1-bits/number-of-1-bits.cs
+++ number-of-1-bits/number-of-1-bits.cs
@@ -1,5 +1,5 @@
 public class Solution {
     public int HammingWeight(int n) {
-        return (int)System.Runtime.Intrinsics.X86.Popcnt.PopCount((uint)n);
+        return (int)System.Runtime.Intrinsics.X86.Popcnt.X64.PopCount((uint)n);
     }
 }
