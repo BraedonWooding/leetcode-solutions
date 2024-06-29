97a4eca019c04d714e1a11184556254dbec6df9a
Braedon <Braedonww@gmail.com>
2024-06-30 00:43:41 +1000

    Time: 75 ms (72.97%) | Memory: 42.9 MB (23.10%) - LeetSync

--- triangle/triangle.cs
+++ triangle/triangle.cs
@@ -1,5 +1,25 @@
 public class Solution {
+    // we can optimize this using prev/current rows
+    // given that it's a 1D translatable problem
     public int MinimumTotal(IList<IList<int>> triangle) {
+        var prevRow = new int[triangle.Last().Count()];
+        var currentRow = new int[triangle.Last().Count()];
+        prevRow[0] = triangle[0][0];
+        
+        for (int i = 1; i < triangle.Count(); i++) {
+            for (int j = 0; j < triangle[i].Count(); j++) {
+                currentRow[j] = Math.Min(
+                    j < triangle[i - 1].Count() ? prevRow[j] : int.MaxValue,
+                    j > 0 ? prevRow[j - 1] : int.MaxValue
+                ) + triangle[i][j];
+            }
+            (prevRow, currentRow) = (currentRow, prevRow);
+        }
+
+        return prevRow.Min();
+    }
+
+    public int MinimumTotal2D(IList<IList<int>> triangle) {
         var dp = new int[triangle.Count()][];
         // base case, first array is always length 0
         dp[0] = new int[] { triangle[0][0] };
