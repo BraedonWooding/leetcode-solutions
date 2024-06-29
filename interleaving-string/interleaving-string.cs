0626d8abfade6466b9963c0e1192cbbb62ec308c
Braedon <Braedonww@gmail.com>
2024-06-29 17:33:05 +1000

    Time: 84 ms (6.58%) | Memory: 43.9 MB (9.21%) - LeetSync

---
+++ interleaving-string/interleaving-string.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public bool IsInterleave(string s1, string s2, string s3) {
+        if (s1.Length + s2.Length != s3.Length) return false;
+        var dp = new bool[s1.Length + 1, s2.Length + 1];
+        // base case empty strings work
+        dp[0, 0] = true;
+        for (int i = 1; i <= s1.Length && s3[i - 1] == s1[i - 1]; i++) {
+            dp[i, 0] = true;
+        }
+        for (int i = 1; i <= s2.Length && s3[i - 1] == s2[i - 1]; i++) {
+            dp[0, i] = true;
+        }
+
+        for (int i = 1; i <= s1.Length; i++) {
+            for (int j = 1; j <= s2.Length; j++) {
+                if (s3[i + j - 1] == s1[i - 1]) {
+                    dp[i, j] = dp[i - 1, j];
+                }
+                if (s3[i + j - 1] == s2[j - 1]) {
+                    dp[i, j] |= dp[i, j - 1];
+                }
+            }
+        }
+
+        for (int i = 0; i <= s1.Length; i++) {
+            for (int j = 0; j <= s2.Length; j++) {
+                Console.Write((dp[i, j] ? 1 : 0) + " ");
+            }
+            Console.WriteLine();
+        }
+
+        return dp[s1.Length, s2.Length];
+    }
+}
