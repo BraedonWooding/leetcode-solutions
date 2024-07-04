0f3ef21987dfa5d112765d47a7d4479abc368127
Braedon <Braedonww@gmail.com>
2024-07-05 01:26:34 +1000

    Time: 1910 ms (5.09%) | Memory: 54.2 MB (6.07%) - LeetSync

--- longest-increasing-subsequence/longest-increasing-subsequence.cs
+++ longest-increasing-subsequence/longest-increasing-subsequence.cs
@@ -2,14 +2,16 @@ public class Solution {
     public int LengthOfLIS(int[] nums) {
         var dp = new int[nums.Length + 1];
         dp[0] = 0;
+        int max = 0;
         for (int i = 0; i < nums.Length; i++) {
             dp[i + 1] = Enumerable.Range(0, i)
                 .Where(x => nums[x] < nums[i])
                 .Select(x => dp[x + 1] + 1)
                 .Concat(new int[]{1})
                 .Max();
+            if (dp[i + 1] > max) max = dp[i + 1];
         }
 
-        return dp.Max();
+        return max;
     }
 }
