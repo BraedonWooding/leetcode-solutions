7c8eacbce9b6267414399ad72ea1039a32e9da79
Braedon <Braedonww@gmail.com>
2024-07-05 01:53:41 +1000

    Time: 73 ms (37.67%) | Memory: 44.5 MB (13.44%) - LeetSync

---
+++ convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode SortedArrayToBST(int[] nums) {
+        return Helper(nums, 0, nums.Length);
+    }
+
+    public TreeNode Helper(int[] nums, int start, int length) {
+        if (length <= 0) return null;
+        int middle = start + length / 2;
+        Console.WriteLine(start + " : " + length + " = " + middle);
+        return new TreeNode(nums[middle], Helper(nums, start, middle - start), Helper(nums, middle + 1, (start + length) - middle - 1));
+    }
+}
