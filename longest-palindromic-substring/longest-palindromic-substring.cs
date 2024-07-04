8380e347264abe61ad157d921d18d664e23646b7
Braedon <Braedonww@gmail.com>
2024-07-05 03:00:54 +1000

    Time: 103 ms (44.08%) | Memory: 70.2 MB (33.72%) - LeetSync

--- longest-palindromic-substring/longest-palindromic-substring.cs
+++ longest-palindromic-substring/longest-palindromic-substring.cs
@@ -1,6 +1,35 @@
 public class Solution {
     public string LongestPalindrome(string s) {
-        return Recursive(s, 0, 0);
+        // 2 variables are your indexes
+        var dp = new int[s.Length, s.Length];
+        int maxLength = 1;
+        int start = 0;
+        // we know that the diagonal are all palindromes (of one)
+        for (int i = 0; i < s.Length; i++) {
+            dp[i, i] = 1;
+        }
+        for (int i = 0; i < s.Length -1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i, i + 1] = 2;
+                start = i;
+                maxLength = 2;
+            }
+        }
+
+        for (int len = 2; len < s.Length; len++) {
+            for (int i = 0; i < s.Length - len; i++) {
+                int end = i + len;
+                if (s[i] == s[end] && dp[i + 1, end - 1] > 0) {
+                    dp[i, end] = dp[i + 1, end - 1] + 1;
+                    if (len + 1 > maxLength) {
+                        start = i;
+                        maxLength = len + 1;
+                    }
+                }
+            }
+        }
+
+        return s[start..(start + maxLength)];
     }
 
     public string Recursive(string s, int start, int end) {
@@ -17,6 +46,8 @@ public class Solution {
                 candidate = possible;                
                 maxLength = possible.Length;
             }
+
+            // odd or even palindrome
 
             if (start > 0 && end < s.Length - 1) {
                 possible = Recursive(s, start - 1, end + 1);
