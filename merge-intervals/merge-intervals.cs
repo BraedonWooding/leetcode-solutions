452ed79e1fefc2e86e872c40880dcd9c95135adc
Braedon <Braedonww@gmail.com>
2024-07-07 02:12:16 +1000

    Time: 138 ms (95.81%) | Memory: 55.2 MB (91.92%) - LeetSync

--- merge-intervals/merge-intervals.cs
+++ merge-intervals/merge-intervals.cs
@@ -1,4 +1,40 @@
 public class Solution {
+    public int[][] Merge2(int[][] intervals) {
+        // to reduce memory first detect how many intervals we have
+        int intervalCount = 0;
+        // first lets sort our intervals
+        Array.Sort(intervals, (a,b) => a[0] - b[0]);
+        int curInterval = 0;
+        int skipped = 0;
+        int curPointer = 0;
+        while (curPointer < intervals.Length - skipped) {
+            if (curInterval == curPointer) {
+                intervalCount++;
+                curPointer++;
+            }
+            else if (intervals[curInterval][1] >= intervals[curPointer][0]) {
+                // update the array
+                intervals[curInterval][1] = Math.Max(intervals[curInterval][1], intervals[curPointer][1]);
+ 
+                // increment skipped so we know how far through we are
+                skipped++;
+
+                for (int i = curPointer; i < intervals.Length - skipped; i++) {
+                    intervals[i] = intervals[i + 1];
+                }
+            }
+            else {
+                // doesn't overlap so reset count
+                intervalCount++;
+                curInterval = curPointer++;
+            }
+        }
+
+        // resize result
+        Array.Resize(ref intervals, intervalCount);
+        return intervals;
+    }
+
     // this has extremely low memory usage (top 99%)
     // because it reuses the passed in array to output the intervals
     // it does this while being semi-efficient O(n) by using a 2-ptr approach
