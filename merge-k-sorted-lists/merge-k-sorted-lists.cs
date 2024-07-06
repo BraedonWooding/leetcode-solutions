7533664db29f7dd802f2f0a38c16a0b72e150559
Braedon <Braedonww@gmail.com>
2024-07-06 23:01:03 +1000

    Time: 198 ms (23.54%) | Memory: 46.1 MB (75.56%) - LeetSync

---
+++ merge-k-sorted-lists/merge-k-sorted-lists.cs
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        ListNode head = null;
+        ListNode cur = null;
+        while (true) {
+            ListNode lowestNode = null;
+            int index = -1;
+            for (int i = 0; i < lists.Length; i++) {
+                if (lists[i] != null && (lowestNode == null || lowestNode.val > lists[i].val)) {
+                    lowestNode = lists[i];
+                    index = i;
+                }
+            }
+
+            // no more items
+            if (lowestNode == null) break;
+            else {
+                lists[index] = lowestNode.next;
+                lowestNode.next = null;
+                if (head == null) head = cur = lowestNode;
+                else cur = cur.next = lowestNode;
+            }
+        }
+
+        return head;
+    }
+}
