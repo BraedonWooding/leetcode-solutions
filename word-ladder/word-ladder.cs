f7117feff36ff6973f44a8842c61ddec2bfdacaf
Braedon <Braedonww@gmail.com>
2024-07-08 00:15:02 +1000

    Time: 115 ms (92.37%) | Memory: 68.7 MB (56.40%) - LeetSync

---
+++ word-ladder/word-ladder.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList) {
+        var set = wordList.ToHashSet();
+
+        // no point looping if we can't even get the last word
+        if (set.Contains(endWord) == false) return 0;
+        if (beginWord == endWord) return 1;
+
+        // precompute all the variations of each word (i.e. remove each character once)
+        // we have to replace each char removed with an _
+        var lookup = new Dictionary<string, List<string>>();
+        foreach (var word in wordList) {
+            for (int i = 0; i < word.Length; i++) {
+                var lookupWord = word[0..i] + "_" + word[(i + 1)..];
+                if (!lookup.TryGetValue(lookupWord, out var words)) words = lookup[lookupWord] = new();
+                words.Add(word);
+            }
+        }
+
+        var queue = new Queue<(string word, int len)>();
+        queue.Enqueue((beginWord, 1));
+
+        while (queue.Count > 0) {
+            var (word, len) = queue.Dequeue();
+            // find all words within 1 char in wordList
+            for (int i = 0; i < word.Length; i++) {
+                if (lookup.TryGetValue(word[0..i] + "_" + word[(i + 1)..], out var words)) {
+                    foreach (var newWord in words) {
+                        if (newWord == endWord) return len + 1;
+                        if (newWord == word || set.Contains(newWord) == false) continue;
+                        queue.Enqueue((newWord, len + 1));
+                        set.Remove(newWord);
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+}
