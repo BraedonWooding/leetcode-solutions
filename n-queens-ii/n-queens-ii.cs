5e556e6f3e8a57b3eb127438f1a0ac9b649869f8
Braedon <Braedonww@gmail.com>
2024-07-07 20:17:36 +1000

    Time: 14 ms (99.15%) | Memory: 26.8 MB (82.63%) - LeetSync

--- n-queens-ii/n-queens-ii.cs
+++ n-queens-ii/n-queens-ii.cs
@@ -1,11 +1,71 @@
 public class Solution {
-    public int TotalNQueens(int n) {
+    public int STotalNQueens(int n) {
         // n <= 9
-        // 9 * 6 (or 54) bits of storage
+        // 9 * 2 + (17 * 2) (or 36) bits of storage
         // 64bit machine so we can efficiently store in 64 bit memory
         // board is structured like this;
         var board = 0UL;
 
+        int Solve(ulong y) {
+            if ((int)y == n) {
+                // we've got a queen in every position
+                return 1;
+            }
+
+            int count = 0;
+
+            for (ulong x = 0; x < (ulong)n; x++) {
+                // 0, 3 = 0
+                // 0, 2 = 1
+                // 0, 1 = 2
+                // 0, 0 = 3
+                //
+                // 3, 3 = 3
+                // 3, 2 = 4
+                // 3, 1 = 5
+                // 3, 0 = 6
+                ulong diagDownCol = (1ul << (int)((x) + (3 - y)));
+                // 3, 3 = 0
+                // 3, 2 = 1
+                // 3, 1 = 2
+                // 3, 0 = 3
+                //
+                // 0, 0 = 6
+                ulong diagUpCol = (1ul << (int)((3 - y) + (3 - x)));
+
+                ulong col = (1ul << (int)(y));
+                ulong row = (1ul << (int)(x));
+
+                var newBoardState = (
+                    row |
+                    (col << 10) |
+                    (diagUpCol << 24) |
+                    (diagDownCol << 44)
+                );
+
+                if (
+                    ((board & row) == 0) &&
+                    (((board >> 10) & col) == 0) &&
+                    (((board >> 24) & diagUpCol) == 0) &&
+                    (((board >> 44) & diagDownCol) == 0)
+                ) {
+                    if ((board & newBoardState) != 0) {
+                        Console.WriteLine("HUH");
+                    }
+
+                    board |= newBoardState;
+                    count += Solve(y + 1);
+                    board &= ~newBoardState;
+                }
+            }
+
+            return count;
+        }
+
+        return Solve(0);
+    }
+
+    public int TotalNQueens(int n) {
         // up to 9 bits of storage so we can just store in 16 bits easily
         // keeping as 32 bits since more efficient on system
         var rows = 0u;
