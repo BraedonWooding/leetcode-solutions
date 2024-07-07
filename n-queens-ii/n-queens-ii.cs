6d298e21f89113b182c4d610881b6947398ba3e9
Braedon <Braedonww@gmail.com>
2024-07-07 20:27:26 +1000

    Time: 30 ms (40.25%) | Memory: 26.7 MB (90.68%) - LeetSync

--- n-queens-ii/n-queens-ii.cs
+++ n-queens-ii/n-queens-ii.cs
@@ -16,32 +16,11 @@ public class Solution {
             int count = 0;
 
             for (int x = 0; x < len; x++) {
-                // 0, 3 = 0
-                // 0, 2 = 1
-                // 0, 1 = 2
-                // 0, 0 = 3
-                //
-                // 3, 3 = 3
-                // 3, 2 = 4
-                // 3, 1 = 5
-                // 3, 0 = 6
-                ulong diagDownCol = (1ul << (34 + (x) + (maxDiags - y)));
-                // 3, 3 = 0
-                // 3, 2 = 1
-                // 3, 1 = 2
-                // 3, 0 = 3
-                //
-                // 0, 0 = 6
-                ulong diagUpCol = (1ul << (17 + (maxDiags - y) + (maxDiags - x)));
-
-                ulong col = (1ul << (y + 9));
-                ulong row = (1ul << x);
-
                 ulong newBoardState = (
-                    row |
-                    (col) |
-                    (diagUpCol) |
-                    (diagDownCol)
+                    (1ul << x) | // row
+                    (1ul << (y + 9)) | // col
+                    (1ul << (17 + (maxDiags - y) + (maxDiags - x))) | // diag up
+                    (1ul << (34 + (x) + (maxDiags - y))) // diag down
                 );
 
                 if ((board & newBoardState) == 0) {
