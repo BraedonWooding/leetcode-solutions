48fa21b190169a0fb3e6f8ad4f1862927c5848cc
Braedon <Braedonww@gmail.com>
2024-07-08 01:10:12 +1000

    Time: 499 ms (11.58%) | Memory: 163.3 MB (7.78%) - LeetSync

--- combinations/combinations.cs
+++ combinations/combinations.cs
@@ -2,7 +2,7 @@ public class Solution {
     public IList<IList<int>> Combine(int n, int k) {
         var results = new List<IList<int>>();
 
-        void CombineBT(List<int> items, int start, int n, int k) {
+        void CombineBT(Stack<int> items, int start, int n, int k) {
             if (k == 0) {
                 // we can choose no more items
                 results.Add(new List<int>(items));
@@ -10,9 +10,9 @@ public class Solution {
             }
 
             for (int i = start; i <= n; i++) {
-                items.Add(i);
+                items.Push(i);
                 CombineBT(items, i + 1, n, k - 1);
-                items.Remove(i);
+                items.Pop();
             }
         }
 
